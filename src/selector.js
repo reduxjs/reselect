e2ed22fd87b1577a75ee362ca01a2feed8e59ddf
faassen <faassen@startifact.com>
2015-07-01 13:04:51 +0200

    Sketch of what the code might look like.

---
+++ src/selector.js
@@ -0,0 +1,34 @@
+function createSelectorCreator(valueEquals) {
+    return (...args) => {
+        if (args.length === 1) {
+            throw Error("Need to pass in multiple arguments");
+        }
+        const selectors = args.slice(0, -1);
+        const resultFunc = args[args.length - 1];
+        const memoizedResultFunc = memoize(resultFunc, valueEquals);
+        return state => {
+            const params = selectors.map(selector => selector(state));
+            return memoizedResultFunc(...params);
+        }
+    };
+}
+
+function memoize(func, valueEquals) {
+    let lastArgs = null;
+    let lastResult = null;
+    return (...args) => {
+        if (lastArgs !== null && argsEquals(args, lastArgs, valueEquals)) {
+            return lastResult;
+        }
+        lastArgs = args;
+        lastResult = func(...args);
+        return lastResult;
+    }
+}
+
+function argsEquals(a, b, valueEquals) {
+    if (a.length != b.length) {
+        return False;
+    }
+    return a.every((value, index) => valueEquals(value, b[index]));
+}
