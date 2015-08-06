0d903647865aab32d7cf388706133a466b0586e3
Lee Bannard <l_bannard@yahoo.co.uk>
2015-08-06 17:06:16 +0100

    make memoize exportable

--- test/test_selector.js
+++ test/test_selector.js
@@ -1,5 +1,5 @@
 import chai from 'chai';
-import {createSelector, createSelectorCreator} from '../src/index';
+import { createSelector, createSelectorCreator, memoize } from '../src/index';
 
 let assert = chai.assert;
 
@@ -65,6 +65,34 @@ suite('selector', function() {
         assert.equal(selector({a: 2}), 1); // yes, really true
         assert.equal(called, 1);
         assert.equal(selector({a: 'A'}), 'A');
+        assert.equal(called, 2);
+    });
+    test("exported memoize", function() {
+        let called = 0;
+        const memoized = memoize(state => {
+            called++;
+            return state.a;
+        });
+        const o1 = {a: 1};
+        const o2 = {a: 2};
+        assert.equal(memoized(o1), 1);
+        assert.equal(memoized(o1), 1);
+        assert.equal(called, 1);
+        assert.equal(memoized(o2), 2);
+        assert.equal(called, 2);
+    });
+    test("exported memoize with valueEquals override", function() {
+        // a rather absurd equals operation we can verify in tests
+        const valueEquals = (a, b) => typeof a === typeof b;
+        let called = 0;
+        const memoized = memoize(a => {
+            called++;
+            return a;
+        }, valueEquals);
+        assert.equal(memoized(1), 1);
+        assert.equal(memoized(2), 1); // yes, really true
+        assert.equal(called, 1);
+        assert.equal(memoized('A'), 'A');
         assert.equal(called, 2);
     });
 });
