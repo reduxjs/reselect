7a3523f202b757b661456fa74a910e1275425db9
Lee Bannard <l_bannard@yahoo.co.uk>
2015-10-26 17:55:22 +0000

    fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,82 +1,82 @@
 function defaultEqualityCheck(a, b) {
-    return a === b;
+  return a === b
 }
 
 export function defaultMemoize(func, equalityCheck = defaultEqualityCheck) {
-    let lastArgs = null;
-    let lastResult = null;
-    return (...args) => {
-        if (lastArgs !== null &&
-            args.every((value, index) => equalityCheck(value, lastArgs[index]))) {
-            return lastResult;
-        }
-        lastArgs = args;
-        lastResult = func(...args);
-        return lastResult;
-    };
+  let lastArgs = null
+  let lastResult = null
+  return (...args) => {
+    if (lastArgs !== null &&
+        args.every((value, index) => equalityCheck(value, lastArgs[index]))) {
+      return lastResult
+    }
+    lastArgs = args
+    lastResult = func(...args)
+    return lastResult
+  }
 }
 
 function getDependencies(funcs) {
-    const dependencies = Array.isArray(funcs[0]) ? funcs[0] : funcs;
+  const dependencies = Array.isArray(funcs[0]) ? funcs[0] : funcs
 
-    if (!dependencies.every(dep => typeof dep === 'function')) {
-        const dependencyTypes = dependencies.map(
-            dep => typeof dep
-        ).join(', ');
-        throw new Error(
-            `Selector creators expect all input-selectors to be functions, ` +
-            `instead received the following types: [${dependencyTypes}]`
-        );
-    }
+  if (!dependencies.every(dep => typeof dep === 'function')) {
+    const dependencyTypes = dependencies.map(
+      dep => typeof dep
+    ).join(', ')
+    throw new Error(
+      `Selector creators expect all input-selectors to be functions, ` +
+        `instead received the following types: [${dependencyTypes}]`
+    )
+  }
 
-    return dependencies;
+  return dependencies
 }
 
 export function createSelectorCreator(memoize, ...memoizeOptions) {
-    return (...funcs) => {
-        let recomputations = 0;
-        const resultFunc = funcs.pop();
-        const dependencies = getDependencies(funcs);
+  return (...funcs) => {
+    let recomputations = 0
+    const resultFunc = funcs.pop()
+    const dependencies = getDependencies(funcs)
 
-        const memoizedResultFunc = memoize(
-            (...args) => {
-                recomputations++;
-                return resultFunc(...args);
-            },
-            ...memoizeOptions
-        );
+    const memoizedResultFunc = memoize(
+      (...args) => {
+        recomputations++
+        return resultFunc(...args)
+      },
+      ...memoizeOptions
+    )
 
-        const selector = (state, props, ...args) => {
-            const params = dependencies.map(
-                dependency => dependency(state, props, ...args)
-            );
-            return memoizedResultFunc(...params);
-        };
+    const selector = (state, props, ...args) => {
+      const params = dependencies.map(
+        dependency => dependency(state, props, ...args)
+      )
+      return memoizedResultFunc(...params)
+    }
 
-        selector.recomputations = () => recomputations;
-        return selector;
-    };
+    selector.recomputations = () => recomputations
+    return selector
+  }
 }
 
 export function createSelector(...args) {
-    return createSelectorCreator(defaultMemoize)(...args);
+  return createSelectorCreator(defaultMemoize)(...args)
 }
 
 export function createStructuredSelector(selectors, selectorCreator = createSelector) {
-    if (typeof selectors !== 'object') {
-        throw new Error(
-            `createStructuredSelector expects first argument to be an object ` +
-            `where each property is a selector, instead received a ${typeof selectors}`
-        );
+  if (typeof selectors !== 'object') {
+    throw new Error(
+      `createStructuredSelector expects first argument to be an object ` +
+        `where each property is a selector, instead received a ${typeof selectors}`
+    )
+  }
+  const objectKeys = Object.keys(selectors)
+  return selectorCreator(
+    objectKeys.map(key => selectors[key]),
+    (...values) => {
+      return values.reduce((composition, value, index) => {
+        composition[objectKeys[index]] = value
+        return composition
+      }, {})
     }
-    const objectKeys = Object.keys(selectors);
-    return selectorCreator(
-        objectKeys.map(key => selectors[key]),
-        (...values) => {
-            return values.reduce((composition, value, index) => {
-                composition[objectKeys[index]] = value;
-                return composition;
-            }, {});
-        }
-    );
+  )
 }
