9bda1cc066fe94364431895e88c2bfe09d1f3cfd
threehams <threehams@gmail.com>
2016-06-28 21:17:25 -0700

    issue/136 Support array argument for createSelector

--- typescript_test/should_compile/index.ts
+++ typescript_test/should_compile/index.ts
@@ -1,7 +1,6 @@
 import {createSelector} from '../../src/reselect.d.ts';
 import * as common from '../common';
 
-
 // Explicitly typed.
 const explicitlyTypedSelector = createSelector<
   common.RootState, common.DeleteButtonContainerProps, common.DeleteButtonStateProps,
@@ -9,7 +8,7 @@ const explicitlyTypedSelector = createSelector<
 > (
   (state, props) => !!state.items[props.itemId],
   (itemExists: boolean) => ({
-    disabled: !itemExists
+    disabled: !itemExists,
   })
 );
 common.selectorConsumer(explicitlyTypedSelector);
@@ -18,14 +17,13 @@ explicitlyTypedSelector(
   {
     itemId: 'abcd',
   }
-)
-
+);
 
 // Implicitly typed.
 const implicitlyTypedSelector = createSelector(
   (state: common.RootState, props: common.DeleteButtonContainerProps) => !!state.items[props.itemId],
   (itemExists: boolean) => ({
-    disabled: !itemExists
+    disabled: !itemExists,
   })
 );
 common.selectorConsumer(implicitlyTypedSelector);
@@ -34,4 +32,47 @@ implicitlyTypedSelector(
   {
     itemId: 'abcd',
   }
-)
+);
+
+// Array syntax
+const arrayType = createSelector(
+  [
+    (state: common.RootState, props: common.DeleteButtonContainerProps) => !!state.items[props.itemId],
+  ],
+  (itemExists: boolean) => ({
+    disabled: !itemExists,
+  })
+);
+common.selectorConsumer(arrayType);
+arrayType(
+  common.rootState,
+  {
+    itemId: 'abcd',
+  }
+);
+
+// Array syntax, 5 inputs
+// Allows for possible refactoring with upcoming TypeScript 2.1 object spread operator
+const arrayTypeFive = createSelector(
+  [
+    (state: common.RootState, props: common.DeleteButtonContainerProps) => !!state.items[props.itemId],
+    (state: common.RootState) => !!state.items,
+    (state: common.RootState) => state.items[0],
+    (state: common.RootState, props: common.DeleteButtonContainerProps) => state.items[props.itemId],
+    (state: common.RootState) => !!state.items[0],
+  ],
+  (a: boolean, b: boolean, c: {id: string}, d: {id: string}, e: boolean) => ({
+    a,
+    b,
+    c,
+    d,
+    e,
+  })
+);
+common.selectorConsumer(arrayType);
+arrayTypeFive(
+  common.rootState,
+  {
+    itemId: 'abcd',
+  }
+);
