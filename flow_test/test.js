6ba77971cfec072ad7357bc9ee925a9d8745821c
Lee Bannard <l_bannard@yahoo.co.uk>
2016-07-13 20:31:02 +0100

    Add flow-typed folder to flow test folder

--- flow_test/test.js
+++ flow_test/test.js
@@ -6,9 +6,12 @@ const { createSelector } = reselect;
 const s = createSelector(
   (state: {x: number}) : number => state.x,
   (state: {y: number}) : number => state.y,
-  (x: number, y: number) => {
+  (x, y) => {
     return x + y
   }
 )
 
-const a = s({x: 100, y: 200})
+const a = s({x: 100, y: "200"})
+
+// Packaging issues
+// https://github.com/facebook/nuclide/issues/529
