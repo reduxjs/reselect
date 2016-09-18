113df98827d1b2206652041cc80d29b1c29327ca
Peter Petrov <onestone@gmail.com>
2016-09-18 19:59:44 +0300

    Improve defaultMemoize performance

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ function defaultEqualityCheck(a, b) {
 export function defaultMemoize(func, equalityCheck = defaultEqualityCheck) {
   let lastArgs = null
   let lastResult = null
+  const isEqual = (value, index) => equalityCheck(value, lastArgs[index])
   return (...args) => {
     if (
       lastArgs === null ||
       lastArgs.length !== args.length ||
-      !args.every((value, index) => equalityCheck(value, lastArgs[index]))
+      !args.every(isEqual)
     ) {
       lastResult = func(...args)
     }
