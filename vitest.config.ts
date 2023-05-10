695c36d769b384976e5dd546cce5c9d60e84e6b8
Mark Erikson <mark@isquaredsoftware.com>
2023-05-09 22:54:17 -0400

    Rename test file in prep for splitting up

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    include: ['./test/test_selector.ts'],
+    include: ['./test/**/*.(spec|test).[jt]s?(x)'],
     alias: {
-      'reselect': './src/index.ts', // @remap-prod-remove-line
+      reselect: './src/index.ts', // @remap-prod-remove-line
 
       // this mapping is disabled as we want `dist` imports in the tests only to be used for "type-only" imports which don't play a role for jest
       '@internal/': './src/'
