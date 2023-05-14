3318bf271bc2b2fdfed220e9d9448bb76b8f0e34
Mark Erikson <mark@isquaredsoftware.com>
2023-05-14 18:07:07 -0400

    Build browser ESM and Webpack 4 compat artifacts, and add sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -18,10 +18,12 @@ export default defineConfig(options => {
     entry: {
       reselect: 'src/index.ts'
     },
+    sourcemap: true,
     ...options
   }
 
   return [
+    // Modern ESM
     {
       ...commonOptions,
       format: ['esm'],
@@ -29,8 +31,6 @@ export default defineConfig(options => {
       dts: true,
       clean: true,
       async onSuccess() {
-        // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
-        fs.copyFileSync('dist/reselect.mjs', 'dist/reselect.legacy-esm.js')
         console.log('onSuccess')
 
         console.log('Generating TS 4.6 types...')
@@ -41,6 +41,31 @@ export default defineConfig(options => {
         )
         console.log('TS 4.6 types done')
       }
+    },
+
+    // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
+    // and optional chaining compiled away
+    {
+      ...commonOptions,
+      entry: {
+        'reselect.legacy-esm': 'src/index.ts'
+      },
+      format: ['esm'],
+      outExtension: () => ({ js: '.js' }),
+      target: 'es2017'
+    },
+    // Browser-ready ESM, production + minified
+    {
+      ...commonOptions,
+      entry: {
+        'reselect.browser': 'src/index.ts'
+      },
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production')
+      },
+      format: ['esm'],
+      outExtension: () => ({ js: '.mjs' }),
+      minify: true
     },
     {
       ...commonOptions,
