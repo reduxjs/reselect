f7ff0a1f16c61ec8708057a5fe04e14e2fff2638
Arya Emami <aryaemami59@yahoo.com>
2023-12-17 15:25:10 -0600

    Add type tests for `TypedStructuredSelectorCreator`

--- type-tests/createStructuredSelector.test-d.ts
+++ type-tests/createStructuredSelector.test-d.ts
@@ -1,10 +1,20 @@
-import type { TypedStructuredSelectorCreator } from 'reselect'
-import { createStructuredSelector } from 'reselect'
-import { describe, test } from 'vitest'
+import type { Selector, TypedStructuredSelectorCreator } from 'reselect'
+import { createStructuredSelector, weakMapMemoize } from 'reselect'
+import { describe, expectTypeOf, test } from 'vitest'
+
+interface Todo {
+  id: number
+  completed: boolean
+}
+
+interface Alert {
+  id: number
+  read: boolean
+}
 
 interface RootState {
-  todos: { id: number; completed: boolean }[]
-  alerts: { id: number; read: boolean }[]
+  todos: Todo[]
+  alerts: Alert[]
 }
 
 const state: RootState = {
@@ -18,26 +28,143 @@ const state: RootState = {
   ]
 }
 
-describe('createStructuredSelector', () => {
+describe(createStructuredSelector, () => {
   test('TypedStructuredSelectorCreator', () => {
     const typedStructuredSelectorCreator: TypedStructuredSelectorCreator<RootState> =
       createStructuredSelector
+
     const structuredSelector = typedStructuredSelectorCreator({
       todos: state => state.todos,
       alerts: state => state.alerts
     })
-    structuredSelector(state).alerts
-    structuredSelector(state).todos
+
+    const { todos, alerts } = structuredSelector(state)
+
+    expectTypeOf(todos).toEqualTypeOf<Todo[]>()
+
+    expectTypeOf(alerts).toEqualTypeOf<Alert[]>()
+
+    expectTypeOf(structuredSelector.argsMemoize).toEqualTypeOf(weakMapMemoize)
+
+    expectTypeOf(structuredSelector.memoize).toEqualTypeOf(weakMapMemoize)
+
+    expectTypeOf(structuredSelector.clearCache).returns.toBeVoid()
+
+    expectTypeOf(structuredSelector.clearCache).parameters.toEqualTypeOf<[]>()
+
+    expectTypeOf(structuredSelector.dependencies).items.toBeFunction()
+
+    expectTypeOf(structuredSelector.dependencyRecomputations).toEqualTypeOf<
+      () => number
+    >()
+
+    expectTypeOf(structuredSelector.recomputations).toEqualTypeOf<
+      () => number
+    >()
+
+    expectTypeOf(
+      structuredSelector.resetDependencyRecomputations
+    ).toEqualTypeOf<() => void>()
+
+    expectTypeOf(structuredSelector.resetRecomputations).toEqualTypeOf<
+      () => void
+    >()
+
+    expectTypeOf(structuredSelector.lastResult).returns.toEqualTypeOf(state)
+
+    expectTypeOf(
+      structuredSelector.memoizedResultFunc
+    ).parameters.toEqualTypeOf<[Todo[], Alert[]]>([state.todos, state.alerts])
+
+    expectTypeOf(structuredSelector.memoizedResultFunc).returns.toEqualTypeOf(
+      structuredSelector.lastResult()
+    )
+
+    expectTypeOf(structuredSelector.memoizedResultFunc).toHaveProperty(
+      'clearCache'
+    )
+
+    expectTypeOf(structuredSelector.resultFunc).returns.toEqualTypeOf(
+      structuredSelector.lastResult()
+    )
   })
-  test('parametric', () => {
+
+  test('supports additional parameters', () => {
     const structuredSelector = createStructuredSelector({
       todos: (state: RootState) => state.todos,
       alerts: (state: RootState) => state.alerts,
       todoById: (state: RootState, id: number) => state.todos[id]
     })
-    structuredSelector(state, 0).alerts
-    structuredSelector(state, 0).todoById.id
-    structuredSelector(state, 0).todos
+
     const { alerts, todos, todoById } = structuredSelector(state, 0)
+
+    expectTypeOf(todos).toEqualTypeOf<Todo[]>()
+
+    expectTypeOf(alerts).toEqualTypeOf<Alert[]>()
+
+    expectTypeOf(todoById).toEqualTypeOf<Todo>()
+
+    expectTypeOf(structuredSelector.argsMemoize).toEqualTypeOf(weakMapMemoize)
+
+    expectTypeOf(structuredSelector.memoize).toEqualTypeOf(weakMapMemoize)
+
+    expectTypeOf(structuredSelector.clearCache).returns.toBeVoid()
+
+    expectTypeOf(structuredSelector.clearCache).parameters.toEqualTypeOf<[]>()
+
+    expectTypeOf(structuredSelector.dependencies).items.toMatchTypeOf<
+      Selector<RootState>
+    >()
+
+    expectTypeOf(structuredSelector.dependencyRecomputations).toEqualTypeOf<
+      () => number
+    >()
+
+    expectTypeOf(structuredSelector.recomputations).toEqualTypeOf<
+      () => number
+    >()
+
+    expectTypeOf(
+      structuredSelector.resetDependencyRecomputations
+    ).returns.toBeVoid()
+
+    expectTypeOf(
+      structuredSelector.resetDependencyRecomputations
+    ).parameters.items.toBeNever()
+
+    expectTypeOf(structuredSelector.resetRecomputations).returns.toBeVoid()
+
+    expectTypeOf(
+      structuredSelector.resetRecomputations
+    ).parameters.items.toBeNever()
+
+    // Use `.branded` for intersection types https://github.com/mmkal/expect-type#why-is-my-assertion-failing
+    expectTypeOf(structuredSelector.lastResult).returns.branded.toEqualTypeOf<
+      RootState & { todoById: Todo }
+    >()
+
+    expectTypeOf(
+      structuredSelector.memoizedResultFunc
+    ).parameters.toEqualTypeOf<[Todo[], Alert[], Todo]>([
+      state.todos,
+      state.alerts,
+      state.todos[0]
+    ])
+
+    expectTypeOf(structuredSelector.resultFunc).parameters.toEqualTypeOf<
+      [Todo[], Alert[], Todo]
+    >([state.todos, state.alerts, state.todos[0]])
+
+    expectTypeOf(structuredSelector.memoizedResultFunc).returns.toEqualTypeOf(
+      structuredSelector.lastResult()
+    )
+
+    expectTypeOf(structuredSelector.memoizedResultFunc).toHaveProperty(
+      'clearCache'
+    )
+
+    expectTypeOf(structuredSelector.resultFunc).returns.toEqualTypeOf(
+      structuredSelector.lastResult()
+    )
   })
 })
