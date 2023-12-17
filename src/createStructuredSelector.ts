c034d0f11082661adaf942a4c34098e52c6768c3
Arya Emami <aryaemami59@yahoo.com>
2023-12-17 12:56:39 -0600

    Add component example in JSDocs for `createStructuredSelector`

--- src/createStructuredSelector.ts
+++ src/createStructuredSelector.ts
@@ -143,6 +143,39 @@ export interface StructuredSelectorCreator {
    * ```
    *
    * @example
+   * <caption>In your component:</caption>
+   * ```tsx
+   * import type { RootState } from 'createStructuredSelector/modernUseCase'
+   * import { structuredSelector } from 'createStructuredSelector/modernUseCase'
+   * import type { FC } from 'react'
+   * import { useSelector } from 'react-redux'
+   *
+   * interface Props {
+   *   id: number
+   * }
+   *
+   * const MyComponent: FC<Props> = ({ id }) => {
+   *   const { todos, alerts, todoById } = useSelector((state: RootState) =>
+   *     structuredSelector(state, id)
+   *   )
+   *
+   *   return (
+   *     <div>
+   *       Next to do is:
+   *       <h2>{todoById.title}</h2>
+   *       <p>Description: {todoById.description}</p>
+   *       <ul>
+   *         <h3>All other to dos:</h3>
+   *         {todos.map(todo => (
+   *           <li key={todo.id}>{todo.title}</li>
+   *         ))}
+   *       </ul>
+   *     </div>
+   *   )
+   * }
+   * ```
+   *
+   * @example
    * <caption>Simple Use Case</caption>
    * ```ts
    * const selectA = state => state.a
