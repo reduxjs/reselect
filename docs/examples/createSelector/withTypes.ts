40b36b5c13807a62ee3765435eb933d529548525
Arya Emami <aryaemami59@yahoo.com>
2023-12-31 09:04:32 -0600

    Add `createSelector.withTypes` example for docs

---
+++ docs/examples/createSelector/withTypes.ts
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect'
+
+export interface RootState {
+  todos: { id: number; completed: boolean }[]
+  alerts: { id: number; read: boolean }[]
+}
+
+export const createAppSelector = createSelector.withTypes<RootState>()
+
+const selectTodoIds = createAppSelector(
+  [
+    // Type of `state` is set to `RootState`, no need to manually set the type
+    // highlight-start
+    state => state.todos
+    // highlight-end
+  ],
+  todos => todos.map(({ id }) => id)
+)
