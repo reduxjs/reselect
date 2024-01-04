d3464d85fb02f3a8ceeafcbf8326274007947e7e
Arya Emami <aryaemami59@yahoo.com>
2024-01-03 18:34:17 -0600

    Add runtime unit tests for `createStructuredSelector.withTypes`

---
+++ test/createStructuredSelector.withTypes.test.ts
@@ -0,0 +1,27 @@
+import { createStructuredSelector } from 'reselect'
+import type { RootState } from './testUtils'
+import { localTest } from './testUtils'
+
+describe(createStructuredSelector.withTypes, () => {
+  const createTypedStructuredSelector =
+    createStructuredSelector.withTypes<RootState>()
+
+  localTest('should return createStructuredSelector', ({ state }) => {
+    expect(createTypedStructuredSelector.withTypes).to.be.a('function')
+
+    expect(createTypedStructuredSelector.withTypes().withTypes).to.be.a(
+      'function'
+    )
+
+    expect(createTypedStructuredSelector).toBe(createStructuredSelector)
+
+    const structuredSelector = createTypedStructuredSelector({
+      todos: state => state.todos,
+      alerts: state => state.alerts
+    })
+
+    expect(structuredSelector).toBeMemoizedSelector()
+
+    expect(structuredSelector(state)).to.be.an('object').that.is.not.empty
+  })
+})
