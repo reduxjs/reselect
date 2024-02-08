b3563bd6fca3efd403a808452db34a21203068bc
Arya Emami <aryaemami59@yahoo.com>
2024-02-08 10:30:29 -0600

    Remove `expectType` and `expectExactType`

--- typescript_test/typesTestUtils.ts
+++ typescript_test/typesTestUtils.ts
@@ -1,29 +1,5 @@
-export function expectType<T>(t: T): T {
-  return t
-}
-
-export declare type IsAny<T, True, False = never> = true | false extends (
-  T extends never ? true : false
-)
-  ? True
-  : False
-
-export declare type IsUnknown<T, True, False = never> = unknown extends T
-  ? IsAny<T, False, True>
-  : False
-
-type Equals<T, U> = IsAny<
-  T,
-  never,
-  IsAny<U, never, [T] extends [U] ? ([U] extends [T] ? any : never) : never>
->
-
 export type IsEqual<A, B> = (<G>() => G extends A ? 1 : 2) extends <
   G
 >() => G extends B ? 1 : 2
   ? true
   : false
-
-export function expectExactType<T>(t: T) {
-  return <U extends T>(u: U & Equals<T, U>) => {}
-}
