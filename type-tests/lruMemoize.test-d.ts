09ebd58df221a53207d46a3a6ed5ff4131d16a48
Arya Emami <aryaemami59@yahoo.com>
2024-02-09 04:36:56 -0600

    Move some more `lruMemoize` type tests into its own file

--- type-tests/lruMemoize.test-d.ts
+++ type-tests/lruMemoize.test-d.ts
@@ -1,4 +1,9 @@
-import { lruMemoize } from 'reselect'
+import { groupBy, isEqual } from 'lodash'
+import {
+  createSelectorCreator,
+  lruMemoize,
+  referenceEqualityCheck
+} from 'reselect'
 
 describe('type tests', () => {
   test('lruMemoize', () => {
@@ -23,5 +28,61 @@ describe('type tests', () => {
     const ret2 = memoized2('', ['1', '2'])
     const str: string = ret2.str
     const arr: string[] = ret2.arr
+  })
+
+  test('issue #384', () => {
+    // https://github.com/reduxjs/reselect/issues/384
+
+    function multiArgMemoize<F extends (...args: any[]) => any>(
+      func: F,
+      a: number,
+      b: string,
+      equalityCheck = referenceEqualityCheck
+    ): F {
+      // @ts-ignore
+      return () => {}
+    }
+
+    interface Transaction {
+      transactionId: string
+    }
+
+    const toId = (transaction: Transaction) => transaction.transactionId
+    const transactionsIds = (transactions: Transaction[]) =>
+      transactions.map(toId)
+    const collectionsEqual = (ts1: Transaction[], ts2: Transaction[]) =>
+      isEqual(transactionsIds(ts1), transactionsIds(ts2))
+
+    const createTransactionsSelector = createSelectorCreator(
+      lruMemoize,
+      collectionsEqual
+    )
+
+    const createMultiMemoizeArgSelector = createSelectorCreator(
+      multiArgMemoize,
+      42,
+      'abcd',
+      referenceEqualityCheck
+    )
+
+    const select = createMultiMemoizeArgSelector(
+      (state: { foo: string }) => state.foo,
+      foo => `${foo}!`
+    )
+    // error is not applicable anymore
+    select.clearCache()
+
+    const createMultiMemoizeArgSelector2 = createSelectorCreator(
+      multiArgMemoize,
+      42,
+      // @ts-expect-error
+      referenceEqualityCheck
+    )
+
+    const groupTransactionsByLabel = lruMemoize(
+      (transactions: Transaction[]) =>
+        groupBy(transactions, item => item.transactionId),
+      collectionsEqual
+    )
   })
 })
