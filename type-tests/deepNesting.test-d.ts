1a4f499af755d92e0dd178d6321a2644ac3b6209
Arya Emami <aryaemami59@yahoo.com>
2024-02-09 03:21:20 -0600

    Move "nested selector" type test to `deepNesting.test-d.ts`

--- type-tests/deepNesting.test-d.ts
+++ type-tests/deepNesting.test-d.ts
@@ -402,4 +402,35 @@ describe('deep nesting #525', () => {
       }
     )
   })
+
+  test('nested selector', () => {
+    interface State {
+      foo: string
+      bar: number
+      baz: boolean
+    }
+
+    const selector = createSelector(
+      createSelector(
+        (state: State) => state.foo,
+        (state: State) => state.bar,
+        (foo, bar) => ({ foo, bar })
+      ),
+      (state: State) => state.baz,
+      ({ foo, bar }, baz) => {
+        const foo1: string = foo
+        // @ts-expect-error
+        const foo2: number = foo
+
+        const bar1: number = bar
+        // @ts-expect-error
+        const bar2: string = bar
+
+        const baz1: boolean = baz
+        // @ts-expect-error
+        const baz2: string = baz
+      }
+    )
+  })
+
 })
