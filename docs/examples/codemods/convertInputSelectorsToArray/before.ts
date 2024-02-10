50b9f89bd572dd3bc5947043076b8e2a2520d299
Arya Emami <aryaemami59@yahoo.com>
2024-02-09 20:30:03 -0600

    Add `convertInputSelectorsToArray` codemod before and after example

---
+++ docs/examples/codemods/convertInputSelectorsToArray/before.ts
@@ -0,0 +1,12 @@
+import { createSelector } from 'reselect'
+
+export interface RootState {
+  todos: { id: number; completed: boolean }[]
+  alerts: { id: number; read: boolean }[]
+}
+
+const selectTodoById = createSelector(
+  (state: RootState) => state.todos,
+  (state: RootState, id: number) => id,
+  (todos, id) => todos.find(todo => todo.id === id)
+)
