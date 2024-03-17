ce07c5d888f9c25d456137d2eaf4103ea1127fff
Arya Emami <aryaemami59@yahoo.com>
2024-03-17 04:03:27 -0500

    Reapply "Fix JSDocs for `createStructuredSelector`"
    This reverts commit b3dce8fd4ea8c6b9d39778575996b49ea407c7ad.

--- src/createStructuredSelector.ts
+++ src/createStructuredSelector.ts
@@ -364,6 +364,10 @@ export interface StructuredSelectorCreator<StateType = any> {
  * A convenience function that simplifies returning an object
  * made up of selector results.
  *
+ * @param inputSelectorsObject - A key value pair consisting of input selectors.
+ * @param selectorCreator - A custom selector creator function. It defaults to `createSelector`.
+ * @returns A memoized structured selector.
+ *
  * @example
  * <caption>Modern Use Case</caption>
  * ```ts
