1b3d10d9de6450a4d74865d49c91d0c6714811d6
Elliott Kember <elliott.kember@gmail.com>
2025-01-17 11:21:56 +1300

    Use Immutable from Immer

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,48 @@ import {
   DefaultMemoizeOptions
 } from './defaultMemoize'
 
+/** Imported from Immer to avoid having an implicit dependency in this patch */
+type AnyFunc = (...args: any[]) => any
+type PrimitiveType = number | string | boolean
+type AtomicObject = Function | Promise<any> | Date | RegExp
+export type IfAvailable<T, Fallback = void> =
+	// fallback if any
+	true | false extends (T extends never
+	? true
+	: false)
+		? Fallback // fallback if empty type
+		: keyof T extends never
+		? Fallback // original type
+		: T
+type WeakReferences = IfAvailable<WeakMap<any, any>> | IfAvailable<WeakSet<any>>
+export type WritableDraft<T> = {-readonly [K in keyof T]: Draft<T[K]>}
+export type Draft<T> = T extends PrimitiveType
+	? T
+	: T extends AtomicObject
+	? T
+	: T extends IfAvailable<ReadonlyMap<infer K, infer V>> // Map extends ReadonlyMap
+	? Map<Draft<K>, Draft<V>>
+	: T extends IfAvailable<ReadonlySet<infer V>> // Set extends ReadonlySet
+	? Set<Draft<V>>
+	: T extends WeakReferences
+	? T
+	: T extends object
+	? WritableDraft<T>
+	: T
+export type Immutable<T> = T extends PrimitiveType
+	? T
+	: T extends AtomicObject
+	? T
+	: T extends IfAvailable<ReadonlyMap<infer K, infer V>> // Map extends ReadonlyMap
+	? ReadonlyMap<Immutable<K>, Immutable<V>>
+	: T extends IfAvailable<ReadonlySet<infer V>> // Set extends ReadonlySet
+	? ReadonlySet<Immutable<V>>
+	: T extends WeakReferences
+	? T
+	: T extends object
+	? {readonly [K in keyof T]: Immutable<T[K]>}
+	: T
+
 export { defaultMemoize, defaultEqualityCheck }
 
 export type { DefaultMemoizeOptions }
@@ -171,11 +213,11 @@ export interface CreateSelectorFunction<
   <Selectors extends SelectorArray, Result>(
     ...items: [
       ...Selectors,
-      (...args: SelectorResultArray<Selectors>) => Readonly<Result>
+      (...args: SelectorResultArray<Selectors>) => Immutable<Result>
     ]
   ): OutputSelector<
     Selectors,
-    Readonly<Result>,
+    Immutable<Result>,
     (...args: SelectorResultArray<Selectors>) => Result,
     GetParamsFromSelectors<Selectors>,
     Keys
@@ -191,8 +233,8 @@ export interface CreateSelectorFunction<
     ]
   ): OutputSelector<
     Selectors,
-    Readonly<Result>,
-    ((...args: SelectorResultArray<Selectors>) => Readonly<Result>),
+    Immutable<Result>,
+    (...args: SelectorResultArray<Selectors>) => Result,
     GetParamsFromSelectors<Selectors>,
     Keys
   > &
@@ -201,12 +243,12 @@ export interface CreateSelectorFunction<
   /** Input selectors as a separate array */
   <Selectors extends SelectorArray, Result>(
     selectors: [...Selectors],
-    combiner: (...args: SelectorResultArray<Selectors>) => Readonly<Result>,
+    combiner: (...args: SelectorResultArray<Selectors>) => Immutable<Result>,
     options?: CreateSelectorOptions<MemoizeOptions>
   ): OutputSelector<
     Selectors,
-    Readonly<Result>,
-    (...args: SelectorResultArray<Selectors>) => Readonly<Result>,
+    Immutable<Result>,
+    (...args: SelectorResultArray<Selectors>) => Result,
     GetParamsFromSelectors<Selectors>,
     Keys
   > &
